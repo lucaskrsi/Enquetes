28c8e377e64263d97c88706a4d48141fef41bc38
Lucas Pereira <lucas.silva.code@outlook.com.br>
2024-02-10 23:38:42 -0300

    initial

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma"
+
+export async function createPoll(app: FastifyInstance) {
+    
+
+    app.post('/polls', async (req, res) => {
+        const createPollBody = z.object({
+            title: z.string(),
+            options: z.array(z.string())
+        });
+
+        const { title, options } = createPollBody.parse(req.body);
+
+        const poll = await prisma.poll.create({
+            data: {
+                title,
+                options: {
+                    createMany: {
+                        data: options.map(option =>{
+                            return {title: option};
+                        })
+                    }
+                }
+            }
+        });
+
+        return res.status(201).send({ "pollId": poll.id });
+
+    });
+}
